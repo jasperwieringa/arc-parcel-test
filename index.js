eb03bed49a35a70aa89aee19cca7f3a7b0c1d772
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-25 17:10:48 +0100

    Updated the Arc-Parcel demo.

--- index.js
+++ index.js
@@ -5,4 +5,3 @@ import '@arc-web/components/dist/themes/dark.css';
 import '@arc-web/components/dist/components/container/arc-container.js';
 import '@arc-web/components/dist/components/navbar/arc-navbar.js';
 import '@arc-web/components/dist/components/sidebar/arc-sidebar.js';
-import '@arc-web/components/dist/components/button/arc-button.js';
